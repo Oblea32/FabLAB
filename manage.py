cbbdb8ef2785f23592fe162041d582b5cf0a95ef
Oblea32 <cristobal.ruiz11@inacapmail.cl>
2024-12-16 04:00:55 -0300

    update manage

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'resident_evil.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fablab.resident_evil.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
