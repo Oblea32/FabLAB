8353599c5c592ae42dcb0b482a7227c3ebdda332
Oblea32 <cristobal.ruiz11@inacapmail.cl>
2024-12-16 03:18:48 -0300

    wsgi update

--- resident_evil/wsgi.py
+++ resident_evil/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'resident_evil.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fablab.resident_evil.settings')
 
 application = get_wsgi_application()
 
