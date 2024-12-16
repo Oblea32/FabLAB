b90ae4d90d82f4efc34026bc130a0e02fc45b8e3
Oblea32 <cristobal.ruiz11@inacapmail.cl>
2024-12-16 04:26:50 -0300

    renderhost

--- resident_evil/wsgi.py
+++ resident_evil/wsgi.py
@@ -8,11 +8,15 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
-from django.core.wsgi import get_wsgi_application
+# Asegúrate de que la carpeta raíz 'fablab' esté en el sys.path
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
+# Configura el entorno de Django para usar el archivo settings.py
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fablab.resident_evil.settings')
 
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
 
 app = application
