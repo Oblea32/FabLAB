9acee222c5e9ff19f4671560c7af003d0611efea
Oblea32 <cristobal.ruiz11@inacapmail.cl>
2024-12-16 04:30:29 -0300

    ups

--- resident_evil/wsgi.py
+++ resident_evil/wsgi.py
@@ -14,7 +14,7 @@ import sys
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
 # Configura el entorno de Django para usar el archivo settings.py
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fablab.resident_evil.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'resident_evil.settings')
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
