33158deee2a537ec9aa6658982a6c7f80bfaed72
Oblea32 <cristobal.ruiz11@inacapmail.cl>
2024-12-19 17:23:42 -0300

    onlyrender

--- core/apps.py
+++ core/apps.py
@@ -3,5 +3,4 @@ from django.apps import AppConfig
 
 class CoreConfig(AppConfig):
     name = 'core'
-    if settings.DEBUG is False:  # Solo se aplica en producción
-        path = '/opt/render/project/src/core'  # Ruta específica de Render
+    path = '/opt/render/project/src/core'  # Ruta específica de Render
--- core/settings.py
+++ core/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-++gvp7$-gw(gy^^0epl308s&87b#3@9chqvw55i8=0jqytj#w0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['fablab-b26m.onrender.com','fab-lab-blue.vercel.app', 'localhost', '127.0.0.1']
 
